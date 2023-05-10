73099c9dc771f97bb6a099f3d41a0b6df09dc1ab
sixx chung <sixx.chung@gmail.com>
2023-05-10 18:22:49 +0900

    py_class

---
+++ test/name_card.py
@@ -0,0 +1,13 @@
+name = "kimyuna"
+email = "yunakim@naver.com"
+addr = "seoul"
+
+# 
+def print_biz_card(name, email, addr):
+    print("------------------------------")
+    print(f"Name: {name}")
+    print(f"E-mail: {email}")
+    print(f"Addr: {addr}")
+    print("------------------------------")
+
+print_biz_card(name, emial, addr)
