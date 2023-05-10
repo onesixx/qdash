24ed6b3da0384c5953831112ea3b21532f105f39
sixx chung <sixx.chung@gmail.com>
2023-05-10 18:23:03 +0900

    jj

--- test/name_card.py
+++ test/name_card.py
@@ -2,7 +2,7 @@ name = "kimyuna"
 email = "yunakim@naver.com"
 addr = "seoul"
 
-# 
+# https://wikidocs.net/1076
 def print_biz_card(name, email, addr):
     print("------------------------------")
     print(f"Name: {name}")
