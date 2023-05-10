362e751c9127befbefd3764d21c7626879eed1dc
sixx chung <sixx.chung@gmail.com>
2023-05-11 00:43:09 +0900

    a

--- test/name_card.py
+++ test/name_card.py
@@ -2,6 +2,10 @@ name = "kimyuna"
 email = "yunakim@naver.com"
 addr = "seoul"
 
+name2 = "DusanBack"
+email2 = "dusan.back@naver.com"
+addr2 = "Kyunggi"
+
 # https://wikidocs.net/1076
 def print_biz_card(name, email, addr):
     print("------------------------------")
@@ -10,4 +14,41 @@ def print_biz_card(name, email, addr):
     print(f"Addr: {addr}")
     print("------------------------------")
 
-print_biz_card(name, emial, addr)
+print_biz_card(name, email , addr)
+print_biz_card(name2, email2 , addr2)
+
+class BizCard:
+    def set_info(self, name, email, addr):
+        self.name = name
+        self.email = email
+        self.addr = addr
+    def print_info(self):
+        print("------------------------------")
+        print(f"Name: {self.name}")
+        print(f"E-mail: {self.email}")
+        print(f"Addr: {self.addr}")
+        print("------------------------------")
+
+member1 = BizCard()
+member1.set_info("Yuna Kim", "yunakim@naver.com", "Seoul")
+member1.print_info()
+
+member2 = BizCard()
+member2.set_info("Sarang Lee", "sarang.lee@naver.com", "Kyunggi")
+member2.print_info()
+
+class BizCard:
+    def __init__(self, name, email, addr):
+    #def set_info(self, name, email, addr):
+        self.name = name
+        self.email = email
+        self.addr = addr
+    def print_info(self):
+        print("------------------------------")
+        print(f"Name: {self.name}")
+        print(f"E-mail: {self.email}")
+        print(f"Addr: {self.addr}")
+        print("------------------------------")
+# https://wikidocs.net/1742
+member3 = BizCard("Sarang Lee", "sarang.lee@naver.com", "Kyunggi")
+member3.print_info()
